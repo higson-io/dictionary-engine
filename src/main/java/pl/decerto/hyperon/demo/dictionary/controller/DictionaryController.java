ae81caf6a5957f4a70e8639e10690f7efccc1cf0
Artur Osiak <artur.osiak@decerto.pl>
2019-10-07 13:20:50 +0200

    Change the way dictionary values are returned (endpoint dictionaries/by-code)

--- src/main/java/pl/decerto/hyperon/demo/dictionary/controller/DictionaryController.java
+++ src/main/java/pl/decerto/hyperon/demo/dictionary/controller/DictionaryController.java
@@ -3,24 +3,29 @@ package pl.decerto.hyperon.demo.dictionary.controller;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static pl.decerto.hyperon.demo.dictionary.controller.ControllerResponseFactory.response;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import pl.decerto.hyperon.demo.dictionary.dto.ControllerResponseDto;
-import pl.decerto.hyperon.demo.dictionary.dto.DictionaryDto;
-import pl.decerto.hyperon.demo.dictionary.service.DictionaryService;
-
 import java.util.List;
 import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import pl.decerto.hyperon.demo.dictionary.dto.ControllerResponseDto;
+import pl.decerto.hyperon.demo.dictionary.dto.MultiDictionaryDto;
+import pl.decerto.hyperon.demo.dictionary.dto.SingleDictionaryDto;
+import pl.decerto.hyperon.demo.dictionary.service.DictionaryService;
 
 @RestController
 @RequestMapping("/dictionaries")
 public class DictionaryController {
 
 	static final String ONLY_FOR_KEY_DICT_NOTE = "Works only for dictionaries accessible by key. " +
-			"Produces error result for dictionaries accessible only by context.";
+		"Produces error result for dictionaries accessible only by context.";
 	static final String DICT_CODE_DESC = "code of dictionary";
 	static final String LEVEL_CODE_DESC = "code of selected level";
 
@@ -40,30 +45,30 @@ public class DictionaryController {
 
 	@GetMapping(value = "/by-code", produces = APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Get data from selected dictionary", notes = ONLY_FOR_KEY_DICT_NOTE)
-	public ResponseEntity<ControllerResponseDto<DictionaryDto>> getDictionary(
-			@RequestParam @ApiParam(DICT_CODE_DESC) String dictionary) {
+	public ResponseEntity<ControllerResponseDto<MultiDictionaryDto>> getDictionary(
+		@RequestParam @ApiParam(DICT_CODE_DESC) String dictionary) {
 
 		var dict = dictionaryService.getDictionaryByCode(dictionary);
-		return response(new DictionaryDto(dict));
+		return response(new MultiDictionaryDto(dict));
 	}
 
 	@GetMapping(value = "/by-code/by-level", produces = APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Get single-value dictionary constructed from multi-value dictionary by " +
-			"restricting output to specified output level",
+		"restricting output to specified output level",
 		notes = ONLY_FOR_KEY_DICT_NOTE)
-	public ResponseEntity<ControllerResponseDto<DictionaryDto>> getSubDictionaryForLevel(
-			@RequestParam @ApiParam(DICT_CODE_DESC) String dictionary,
-			@ApiParam(LEVEL_CODE_DESC) @RequestParam String level) {
+	public ResponseEntity<ControllerResponseDto<SingleDictionaryDto>> getSubDictionaryForLevel(
+		@RequestParam @ApiParam(DICT_CODE_DESC) String dictionary,
+		@ApiParam(LEVEL_CODE_DESC) @RequestParam String level) {
 
 		var dict = dictionaryService.getDictionaryLevel(dictionary, level);
-		return response(new DictionaryDto(dict));
+		return response(new SingleDictionaryDto(dict));
 	}
 
 	@GetMapping(value = "/by-code/levels", produces = APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Get list of all output levels for specified dictionary",
-			notes = ONLY_FOR_KEY_DICT_NOTE)
+		notes = ONLY_FOR_KEY_DICT_NOTE)
 	public ResponseEntity<ControllerResponseDto<Set<String>>> getDictionaryLevels(
-			@RequestParam @ApiParam(DICT_CODE_DESC) String dictionary) {
+		@RequestParam @ApiParam(DICT_CODE_DESC) String dictionary) {
 
 		return response(dictionaryService.getDictionaryLevels(dictionary));
 	}
--- src/main/java/pl/decerto/hyperon/demo/dictionary/dict/impl/SingleValueDictionaryEntry.java
+++ src/main/java/pl/decerto/hyperon/demo/dictionary/dict/impl/SingleValueDictionaryEntry.java
@@ -2,6 +2,9 @@ package pl.decerto.hyperon.demo.dictionary.dict.impl;
 
 import java.util.Optional;
 import lombok.Value;
+
+import org.apache.commons.lang3.StringUtils;
+
 import pl.decerto.hyperon.demo.dictionary.dict.DictionaryEntry;
 
 /**
@@ -12,6 +15,11 @@ class SingleValueDictionaryEntry implements DictionaryEntry {
 
 	private final String key;
 	private final String value;
+
+	@Override
+	public String getValueByLevel(String level) {
+		return StringUtils.EMPTY;
+	}
 
 	@Override
 	public Optional<DictionaryEntry> getSubEntry(String level) {
